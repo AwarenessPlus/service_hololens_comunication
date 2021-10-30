b347e1fc8db5d0e47dec523513f233391de370cc
Juan Felipe Marin Florez <jumarin@javeriana.edu.co>
2021-10-30 14:41:43 -0500

    Service created and working with the hololens connection status and procedure id

---
+++ ServiceHololensCommunication/Controllers/HololensCommunicationController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceHololensCommunication.Services;
+
+namespace ServiceHololensCommunication.Controllers
+{
+    [ApiController]
+
+    public class HololensCommunicationController : Controller
+    {
+        private readonly HololensCommunicationService HololensCommunicationService;
+
+        public HololensCommunicationController(HololensCommunicationService hololensCommunicationService)
+        {
+            HololensCommunicationService = hololensCommunicationService;
+        }
+
+        [Route("api/hololens-communication-service/health-status")]
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok("Service connected");
+        }
+
+        [Route("api/hololens-communication-service/connect-hololens")]
+        [HttpGet]
+        public IActionResult ConnectHololens()
+        {
+            try
+            {
+                HololensCommunicationService.ConnectHololens();
+                return Ok("Hololens connected");
+            }
+            catch {
+                return NotFound("It wasn't posible connect the hololens. Try again");
+            }
+            
+        }
+
+        [Route("api/hololens-communication-service/disconnect-hololens")]
+        [HttpGet]
+        public IActionResult DisconnectHololens()
+        {
+            try
+            {
+                HololensCommunicationService.DisconnectHololens();
+                HololensCommunicationService.SetProcedureData(0);
+                return Ok("Hololens disconnected");
+            }
+            catch
+            {
+                return NotFound("It wasn't posible disconnect the hololens. Try again");
+            }
+
+        }
+
+        [Route("api/hololens-communication-service/hololens-connection-status")]
+        [HttpGet]
+        public IActionResult HololensConnectionStatus()
+        {
+            return Ok(HololensCommunicationService.HololensConnectionStatus());
+        }
+
+        [Route("api/hololens-communication-service/obtain-procedure-data")]
+        [HttpGet]
+        public IActionResult ObtainProcedureData()
+        {
+            return Ok(HololensCommunicationService.ObtainProcedureData());
+        }
+
+        [Route("api/hololens-communication-service/set-procedure-data")]
+        [HttpPost]
+        public IActionResult SetProcedureData(int IdProcedure)
+        {
+            try {
+                HololensCommunicationService.SetProcedureData(IdProcedure);
+                return Accepted();
+
+            } catch {
+
+                return NotFound();
+
+            }
+        }
+    }
+}
---
+++ ServiceHololensCommunication/Services/HololensCommunicationService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHololensCommunication.Services
+{
+    public class HololensCommunicationService
+    {
+        private static HololensCommunicationService _instance;
+        private Boolean _hololensConnectionStatus;
+        private int _idProcedimiento; 
+
+        public static HololensCommunicationService GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new HololensCommunicationService();
+            }
+            return _instance;
+        }
+
+        public Boolean ConnectHololens()
+        {
+            _hololensConnectionStatus = true;
+            return true;
+
+        }
+
+        public Boolean DisconnectHololens() {
+            _hololensConnectionStatus = false;
+            return true;
+        }
+
+        public Boolean HololensConnectionStatus() {
+            return _hololensConnectionStatus;
+        }
+
+        public int ObtainProcedureData() {
+            return _idProcedimiento;
+        }
+
+        public void SetProcedureData(int IdProcedimiento) {
+            _idProcedimiento = IdProcedimiento;
+        }
+    }
+}
